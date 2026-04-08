db8ada86fe5e8f46da3f4ad80340b59d46f12215
Anton <soloviev.anton@vsmservice.ru>
2026-04-08 13:56:19 +0300

    Изменено отображение для windows

--- RangeCalcPro/App.xaml.cs
+++ RangeCalcPro/App.xaml.cs
@@ -11,7 +11,17 @@ namespace RangeCalcPro
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new AppShell());
+            Window window = new Window(new AppShell());
+
+            // Настройка размеров окна (работает только на Windows)
+#if WINDOWS
+            window.MinimumWidth = 300;
+            window.MinimumHeight = 400;
+            window.Width = 500;   // ширина
+            window.Height = 800;  // высота
+#endif
+
+            return window;
         }
     }
 }
