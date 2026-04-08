24fa37ec324aeb279b52a672847b6fd4c972b9a9
Anton <soloviev.anton@vsmservice.ru>
2026-04-08 10:41:26 +0300

    Добавьте файлы проекта.

---
+++ RangeCalcPro/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace RangeCalcPro
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 1;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void OnCounterClicked(object? sender, EventArgs e)
+        {
+            if (int.TryParse(InitialCountEntry.Text, out int enteredValue))
+            {
+                count = enteredValue;
+                SemanticScreenReader.Announce($"Count установлен в {count}");
+            }
+            else
+            {
+                DisplayAlert("Ошибка", "Введите целое число", "OK");
+            }
+            count *=5;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
